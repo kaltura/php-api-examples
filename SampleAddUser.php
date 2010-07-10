2182b4b051d3e66fb350096384c6f32f45708d1d
MellowK <MellowK@b58a29cf-3064-46da-94c6-1c29cc75c8e5>
2010-07-10 04:51:12 +0000

    Sample script showing how to add a user
    git-svn-id: http://www.kaltura.org/kalorg/kaltura-api-clients-sample-code/php@741 b58a29cf-3064-46da-94c6-1c29cc75c8e5

---
+++ SampleAddUser.php
@@ -0,0 +1,60 @@
+<?php
+
+// Add a new user to an account
+// Run the SampleListUsers.php script to verify the new user is in
+// the account
+
+// Your Kaltura partner credentials
+define("PARTNER_ID", "nnnnnn");
+define("ADMIN_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+define("USER_SECRET",  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+require_once "KalturaClient.php";
+
+$user = "SomeoneWeKnow";  
+$kconf = new KalturaConfiguration(PARTNER_ID);
+$kclient = new KalturaClient($kconf);
+$ksession = $kclient->session->start(ADMIN_SECRET, $user, KalturaSessionType::ADMIN);
+
+if (!isset($ksession)) {
+	die("Could not establish Kaltura session. Please verify that you are using valid Kaltura partner credentials.");
+}
+
+// Set the response format
+// KALTURA_SERVICE_FORMAT_JSON  json
+// KALTURA_SERVICE_FORMAT_XML   xml
+// KALTURA_SERVICE_FORMAT_PHP   php
+$kconf->format = KalturaClientBase::KALTURA_SERVICE_FORMAT_PHP;
+
+$kclient->setKs($ksession);
+
+$kuser = New KalturaUser;
+$kuser->id = "MadeUpId888";
+//$kuser->partnerId 
+$kuser->screenName = "FredJones";
+$kuser->fullName = "Fred Q. Jones";
+$kuser->email = "FredQQQJones@somezzzzmail.com";
+//$kuser->dateOfBirth 
+//$kuser->country 
+//$kuser->state
+//$kuser->city
+//$kuser->zip 
+//$kuser->thumbnailUrl 
+$kuser->description = "A friendly fellow who likes quality video.";
+//$kuser->tags
+//$kuser->adminTags 
+//$kuser->gender 
+$kuser->status = KalturaUserStatus::ACTIVE;
+//$kuser->createdAt 
+//$kuser->updatedAt 
+//$kuser->partnerData
+//$kuser->indexedPartnerDataInt 
+//$kuser->indexedPartnerDataString 
+//$kuser->storageSize 
+
+$result = $kclient->user->add($kuser);
+
+echo "<h3>The New User</h3>";
+print_r($result);
+	
+?>
